29aba32e152df0c91fb32b9c8e28b14aac57c28b
misaka3 <rksas927@gmail.com>
2021-04-03 00:43:47 +0900

    add basic_text_fields_page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import ProductPage from "./components/pages/ProductPage";
 import HomePage from "./components/pages/HomePage";
+import BasicTextFieldsPage from "./components/pages/BasicTextFieldsPage";
 import FormPropsTextFieldsPage from "./components/pages/FormPropsTextFieldsPage";
 
 const App: React.FC = () => {
@@ -10,6 +11,7 @@ const App: React.FC = () => {
     <Router>
       <Switch>
         <Route path="/products" component={ProductPage} exact />
+        <Route path="/basic_text_fields" component={BasicTextFieldsPage} exact />
         <Route path="/form_props_text_fields" component={FormPropsTextFieldsPage} exact />
         <Route path="/" component={HomePage} exact />
       </Switch>
